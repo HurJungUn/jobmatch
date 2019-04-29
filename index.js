cb22fa3fe19a6757f230cd1d46b1bb0589bbf698
HurJungUn <hju7826@daum.net>
2019-04-29 20:07:49 +0900

    2019.04.29

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const path = require('path');
 const request = require('request');
 const cheerio = require('cheerio');
 const bodyParser = require('body-parser');
+const qs = require('querystring');
+const iconv = require('iconv-lite');
+const charset = require('charset');
+const mysql = require('mysql');
+
+/* mysql 연결부분 */
+const conn = mysql.createConnection({
+    user: "yy_30222",
+    password: "1234",
+    host: "gondr.asuscomm.com"
+});
+
+conn.query("USE yy_30222"); //yy_30222 데이터베이스 사용
+
+
+/* mysql 연결종료 */
 
 let app = express();
 
@@ -37,13 +53,56 @@ app.get('/top20', function(req, res){
         list:list
     });
     });
-})
+});
+
+app.get('/ganyum', function(req, res){
+
+    request("https://gall.dcinside.com/board/lists?id=comic_new1&exception_mode=recommend", function(err, response, body){
+    let list = [];
+    $ = cheerio.load(body);
+    
+    let top20 = $(".ub-content");
+
+    for (let i = 0; i < top20.length; i++) {
+        let msg = $(top20[i]).text();
+        list.push(msg);
+    }
+
+    res.render('dc',{
+        msg:'만갤념글 목록',
+        list:list
+    });
+    });
+});
+
+app.post('/ganyum', function(req, res){
+
+    let word = req.body.word;
+    word = qs.escape(word);
+    let url = "https://gall.dcinside.com/board/lists?id=" + word + "&exception_mode=recommend";
+    request(url, function(err, response, body){
+    let list = [];
+    $ = cheerio.load(body);
+    
+    let top20 = $(".ub-content");
+
+    for (let i = 0; i < top20.length; i++) {
+        let msg = $(top20[i]).text();
+        list.push(msg);
+    }
+
+    res.render('dc',{
+        msg:'념글 목록',
+        list:list
+    });
+    });
+});
 
 app.get('/search', function(req, res){
 
     res.render('search',{list:list = undefined});
 
-})
+});
 
 app.post('/search', function(req, res){
 
@@ -66,7 +125,87 @@ app.post('/search', function(req, res){
     });
     });
 
-})
+});
+
+app.get('/lunch', function(req, res){
+
+    res.render('lunch',{
+
+    });
+});
+
+app.post('/lunch', function(req, res){
+
+    let date = req.body.date;
+    date = date.split("-").join("");
+
+    const options = {
+        url: "http://www.y-y.hs.kr/lunch.view?date=" + date,
+        headers: {
+            'User-Agent': 'Mozilla/5.0'
+        },
+        encoding:null
+    }
+    
+    request(options , function(err, response, body){
+        if(err != null){
+            return;
+        }
+    
+        const enc = charset(response.headers, body);
+        const result = iconv.decode(body, enc);
+    
+        $ = cheerio.load(result);
+        let menu = $(".menuName > span");
+
+        res.render('lunch', {
+            menu : menu.text()
+        });
+    });
+
+});
+
+app.get('/board', function(req, res){
+
+    let sql = "SELECT * FROM board WHERE title LIKE ? ORDER BY id DESC";
+
+    let keyword = "%%";
+    if(req.query.key != undefined){
+        keyword = "%" + req.query.key + "%";
+    }
+    conn.query(sql, [keyword], function(err, result){
+
+        res.render('board', {
+            list:result
+        });
+    });
+
+});
+
+app.get('/board/write', function(req, res){
+
+        res.render('write', {
+            
+        });
+
+});
+
+app.post('/board/write', function(req, res){
+
+    let param = [req.body.title,
+    req.body.content,
+    req.body.writer]
+
+    let sql = "INSERT INTO board (title, content, writer) VALUES(?, ?, ?)";
+
+    conn.query(sql, param, function(err, result){
+
+        if (!err) {
+            res.redirect('/board');
+        }
+    });
+
+});
 
 let server = http.createServer(app);
 server.listen(app.get('port'), function(){
--- req.js
+++ req.js
@@ -1,11 +1,14 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
+const charset = require('charset');
+
 const options = {
     url: "http://www.y-y.hs.kr/lunch.view?date=20190429",
     headers: {
-        'User-Agent': 'Mozilla/5.0',
-        'Content-Type': 'text/html; charset=utf-8'
-    }
+        'User-Agent': 'Mozilla/5.0'
+    },
+    encoding:null
 }
 
 request(options , function(err, res, body){
@@ -14,10 +17,13 @@ request(options , function(err, res, body){
         return;
     }
 
-    $ = cheerio.load(body);
-    let list = $(".menuName > span");
-    let txt = $(list[0]).text();
-    console.log(txt);
+    const enc = charset(res.headers, body);
+    console.log(enc);
+    const result = iconv.decode(body, enc);
+
+    $ = cheerio.load(result);
+    let menu = $(".menuName > span");
+    console.log(menu.text());
 });
 
 // request("http://www.y-y.hs.kr/lunch.view?date=20190429", function(err, res, body){
