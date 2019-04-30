7a4bd128ad05b01c4aaf08ea959b0078c41da663
HurJungUn <hju7826@daum.net>
2019-04-30 21:05:01 +0900

    2019.04.30과제 하기

--- router.js
+++ router.js
@@ -258,4 +258,62 @@ router.get('/datalab', function (req, res) {
 
 });
 
+router.post('/datalab', function (req, res) {
+    
+    if (req.body.sub.indexof(",") != null) {
+        let subs = req.body.sub.split(",");    
+    } else {
+        let subs = req.body.sub;
+    }
+
+    let data = [
+        {
+            "groupName": req.body.title,
+            "keywords": subs
+        }
+    ];
+
+    datalab("2019-02-01", "2019-04-30", "week", data, function (result) {
+        let colors = ["rgb(255, 192, 192)", "rgb(75, 192, 255)", "rgb(75, 255, 128)"];
+
+        let gData = {
+            "labels": [
+
+            ], "datasets": [
+
+            ]
+        };
+
+        let r = result.results;
+
+        for (let i = 0; i < r.length; i++) {
+
+            let item = {
+                "label": r[i].title,
+                "borderColor": colors[i],
+                "fill": false,
+                "lineTension": 0.2,
+                "data": []
+            };
+
+            for (let j = 0; j < r[i].data.length; j++) {
+                item.data.push(r[i].data[j].ratio);
+                if (i == 0) {
+                    let date = r[i].data[j].period;
+                    let arr = date.split("-");
+                    gData.labels.push(arr[1] + arr[2]);
+                }
+                
+            }
+
+            gData.datasets.push(item);
+
+        }
+
+        res.render('datalab', {
+            g:gData
+        });
+    });
+});
+
 module.exports = router;
